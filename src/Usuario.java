51b3f08609ea84d5c3c5f78a604d869e9eb3f208
Lucas <lucas_dev@outlook.com.br>
2023-09-02 13:55:38 -0300

    Criando a Classe Susuario

---
+++ src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+
+        SmartTv smartTv = new SmartTv ();
+
+        System.out.println("A TV está ligada ? " + smartTv.ligada);
+        System.out.println("Qual o canal ? " + smartTv.canal);
+        System.out.println("Qual o volume da Tv ? " + smartTv.volume);
+    }
+}
